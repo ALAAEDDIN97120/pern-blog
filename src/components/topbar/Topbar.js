366bab403d14d50c39322b52a15cb682663ed863
‪Alaaddin yahia <150624261+ALAAEDDIN97120@users.noreply.github.com>
2025-07-07 18:02:57 +0300

    feat: Add Topbar

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,5 +1,40 @@
+import "./topbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFacebook,
+	faInstagram,
+	faLinkedin,
+	faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const Topbar = () => {
-	return <div>Topbar</div>;
+	return (
+		<div className="top">
+			<div className="topLeft">
+				<FontAwesomeIcon className="iconTopbar" icon={faFacebook} />
+				<FontAwesomeIcon className="iconTopbar" icon={faInstagram} />
+				<FontAwesomeIcon className="iconTopbar" icon={faTwitter} />
+				<FontAwesomeIcon className="iconTopbar" icon={faLinkedin} />
+			</div>
+			<div className="topcenter">
+				<ul className="topList">
+					<li className="topListItem">HOME</li>
+					<li className="topListItem">ABOUT</li>
+					<li className="topListItem">CONTACT</li>
+					<li className="topListItem">WRITE</li>
+					<li className="topListItem">LOGOUT</li>
+				</ul>
+			</div>
+			<div className="topRight">
+				<img
+					className="topImg"
+					src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+					alt=""
+				/>
+				<FontAwesomeIcon className="iconTopbar" icon={faSearch} />
+			</div>
+		</div>
+	);
 };
 
 export default Topbar;
